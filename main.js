54912d92ae05004071d1f245f7cf89fa3be91a0b
gitJucy <johnrlucy84@gmail.com>
2017-04-03 00:55:14 -0400

    first-push

---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+      $('#find').hide().delay(3500).fadeIn().delay(500).fadeOut(2000);
+      $('#future').hide().delay(6200).fadeIn();
+      $('#social-buttons').hide().delay(6200).fadeIn(2000);
+    });
