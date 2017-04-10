05256aef56a1355866928ea0a1395182909055eb
gitJucy <johnrlucy84@gmail.com>
2017-04-10 00:04:26 -0400

    started work on lightbox gallery

---
+++ assets/gallery.js
@@ -0,0 +1,13 @@
+
+
+$('#image-list').hide();
+$('#future').click(function(){
+  $('#image-list').fadeIn(500);
+  $('#content').fadeOut(500);
+  $('#name').fadeOut(500);
+  $('#future').fadeOut(500);
+  $('#social-buttons').fadeOut(500);
+
+
+
+})
